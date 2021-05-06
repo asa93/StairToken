983b05eae78f8093634a6de93a8db493429fb00c
Slim <slimane.asa@outlook.com>
2021-05-06 23:44:33 +0200

    update poolDispatch rules

--- truffle_project/test/stairtoken2.js
+++ truffle_project/test/stairtoken2.js
@@ -15,11 +15,11 @@ contract('StairToken', (accounts) => {
 
     //assert.equal((await stairToken.totalSupply()).toNumber(), 1000000, "Error getTotalSupply");
     //
-
-    await stairToken.transfer(accounts[2], 100)
-    await stairToken.transfer(accounts[3], 100)
-    await stairToken.transfer(accounts[4], 100)
-    await stairToken.transfer(accounts[5], 100)
+    
+    await stairToken.transfer(accounts[2], 190)
+    await stairToken.transfer(accounts[3], 180)
+    await stairToken.transfer(accounts[4], 170)
+    await stairToken.transfer(accounts[5], 160)
 
     await stairToken.setPoolAddress(poolAddress)
     await stairToken.transfer(poolAddress, 100)
@@ -27,24 +27,30 @@ contract('StairToken', (accounts) => {
 
     //assert.equal(await stairToken.balanceOf(accounts[1]), 100, "Error transfer1");
 
-    assert.equal((await stairToken.getEligibleHolders()).toNumber(),4,"error eligibleHolders")
+    assert.equal((await stairToken.getEligibleHolders()).toNumber(),5,"error eligibleHolders")
 
-   
-    assert.equal((await stairToken.top20Tokens()).toNumber(),0,"error top20tokens")
-    assert.equal((await stairToken.top50Tokens()).toNumber(),10,"error top50tokens")
-    assert.equal((await stairToken.top100Tokens()).toNumber(),5,"error top100tokens")
+    assert.equal((await stairToken.top20Tokens()).toNumber(),30,"error top20tokens")
+    assert.equal((await stairToken.top50Tokens()).toNumber(),18,"error top50tokens")
+    assert.equal((await stairToken.top100Tokens()).toNumber(),3,"error top100tokens")
+
+
+    assert.equal((await balanceTracker.treeAbove(191)).toNumber(), 1, "Error treeCount should be 3");
+
+    let ownerTokens = (await stairToken.balanceOf(accounts[0])).toNumber();
 
     await stairToken.forceDispatch();
 
-    assert.equal((await stairToken.balanceOf(poolAddress)).toNumber(),40,"pool is not empty")
+    assert.equal((await stairToken.balanceOf(poolAddress)).toNumber(),3,"finalPoolBalance is not correct")
     
 
-    assert.equal((await balanceTracker.treeCount()).toNumber(), 3, "Error treeCount should be 3");
+    assert.equal((await balanceTracker.treeCount()).toNumber(), 5, "Error treeCount should be 3");
+    assert.equal((await balanceTracker.treeAbove(194)).toNumber(), 1, "Error treeCount should be 3");
 
-    
-    assert.equal((await stairToken.balanceOf(accounts[2])).toNumber(),110," accounts 2 balance didn't receive all funds")
-    assert.equal((await stairToken.balanceOf(accounts[3])).toNumber(),100," accounts 3 balance didn't receive all funds")
-    assert.equal((await stairToken.balanceOf(accounts[0])).toNumber(),stairToken.totalSupply()-300+10,"accounts 0 balance error")
+    assert.equal((await stairToken.balanceOf(accounts[0])).toNumber(),ownerTokens+30," accounts owner balance error")
+    assert.equal((await stairToken.balanceOf(accounts[2])).toNumber(),208," accounts 2 balance error")
+    assert.equal((await stairToken.balanceOf(accounts[3])).toNumber(),183," accounts 3 balance error")
+    assert.equal((await stairToken.balanceOf(accounts[4])).toNumber(),173,"accounts 4 balance error")
+    assert.equal((await stairToken.balanceOf(accounts[5])).toNumber(),163,"accounts 5 balance error")
 
   });
 /*
